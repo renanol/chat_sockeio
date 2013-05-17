2b9f9e25a49d497d382d0038c9c508165d7fb45b
Renan Oliveira <ren4n.oliveir4@gmail.com>
2013-05-17 22:25:58 +0000

    alter chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,7 +1,7 @@
 window.onload = function() {
  
     var messages = [];
-    var socket = io.connect('http://192.168.0.102:3700');
+    var socket = io.connect('http://localhost:3700');
     var field = document.getElementById("field");
     var sendButton = document.getElementById("send");
     var content = document.getElementById("content");
